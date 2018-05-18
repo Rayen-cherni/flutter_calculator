fbbaed4ca0e6844d906dfa361546ea50640f75d7
Fabio de Matos <fmatosqg@gmail.com>
2018-05-18 11:45:35 +1000

    Adding haptic feedback and predefined colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(new MyApp());
 
@@ -22,6 +23,7 @@ class MyAppState extends State<MyApp> {
   void numberPressed(int value)  {
     operand = operand * 10 + value;
     setState(() => resultString = operand.toString());
+    HapticFeedback.mediumImpact();
   }
 
   void calc(OperatorFunc operation) {
@@ -34,14 +36,15 @@ class MyAppState extends State<MyApp> {
     operand = 0.0;
     var result = accu.toString();
     setState(() => resultString = result.toString().substring(0, min(10,result.length) ));
+    HapticFeedback.heavyImpact();
   }
 
   List<Widget> buildNumberButtons(int count, int from, int flex) {
     return new Iterable.generate(count, (index) {
       return new Expanded(flex: flex,
         child: new Padding(padding: const EdgeInsets.all(8.0),
-          child: RaisedButton(onPressed: () => numberPressed(from + index),
-              color: const Color(0xffeeffff), highlightColor: const Color(0xffaaddaa),
+          child: RaisedButton(onPressed: () => numberPressed(from + index),shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10.0)),
+              color: Colors.teal[100], highlightColor: Colors.teal[400], splashColor: Colors.teal[800],
               elevation: 8.0, highlightElevation: 12.0,
               child: Text(
                 "${from + index}", style: TextStyle(fontSize: 40.0),)),
@@ -54,9 +57,9 @@ class MyAppState extends State<MyApp> {
     return Expanded(flex: flex,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: RaisedButton(onPressed: () => calc(func),
-            color: const Color(0xffffddff), highlightColor: const Color(0xffffaaff), elevation: 10.0, highlightElevation: 16.0,
-            child: Text(label,style:  new TextStyle(fontSize: 32.0))),
+        child: RaisedButton(onPressed: () => calc(func), shape: new StadiumBorder(),
+            color: Colors.purple[100], highlightColor:  Colors.purple[200], splashColor:  Colors.purple[400],
+            elevation: 10.0, highlightElevation: 16.0, child: Text(label,style:  new TextStyle(fontSize: 32.0))),
       ),
     );
   }
@@ -72,15 +75,15 @@ class MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new SafeArea(
-              child: new Material(color: const Color(0xffffeeff),
+              child: new Material(color: Colors.purple[50],
                 child: Column( crossAxisAlignment: CrossAxisAlignment.end,
                    children: <Widget>[
-            Expanded(child: new Card(child:Row(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.end,
-                children: [Text(resultString,textAlign: TextAlign.right, style: TextStyle(fontSize: 50.0, color: const Color(0xff114411)),)]))),
+            Expanded(child: new Material(type:MaterialType.card,elevation: 4.0, child:Row(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.end,
+                children: [Text(resultString,textAlign: TextAlign.right, style: TextStyle(fontSize: 50.0, color: Colors.teal[900]),)]))),
             buildRow(3,7,1,"/", (accu, dividor)=> accu / dividor , 1),
             buildRow(3,4,1,"x", (accu, dividor)=> accu * dividor , 1),
             buildRow(3,1,1,"-", (accu, dividor)=> accu - dividor , 1),
-            buildRow(1, 0,3,"+", (accu, dividor)=> accu + dividor , 1),
+            buildRow(1,0,3,"+", (accu, dividor)=> accu + dividor , 1),
             Expanded(child:Row(crossAxisAlignment: CrossAxisAlignment.stretch,children: [buildOperatorButton("C", null, 1,),buildOperatorButton("=", (accu, dividor)=> accu, 3)],))
           ],),
         ),
