b4bd92ee06071ca889a01e3b6acef271f6b98cc3
Fabio de Matos <fmatosqg@gmail.com>
2018-05-18 10:48:16 +1000

    Apply some colors on highlight, change elevation and paddings

--- lib/main.dart
+++ lib/main.dart
@@ -25,44 +25,44 @@ class MyAppState extends State<MyApp> {
   }
 
   void calc(OperatorFunc operation) {
-      if (operation == null) // C was pressed
-      {
-        accu = 0.0;
-      }
-      else
-      {
-        accu = queuedOperation != null ? queuedOperation(accu, operand) : operand;
-      }
-      queuedOperation = operation;
-      operand = 0.0;
-      var result = accu.toString();
-      setState(() => resultString = result.toString().substring(0, min(10,result.length) ));
+    if (operation == null) { // C was pressed
+      accu = 0.0;
+    } else {
+      accu = queuedOperation != null ? queuedOperation(accu, operand) : operand;
+    }
+    queuedOperation = operation;
+    operand = 0.0;
+    var result = accu.toString();
+    setState(() => resultString = result.toString().substring(0, min(10,result.length) ));
   }
 
-  List<Widget>  buildNumberButtons( int count,int from, int flex) {
-    return new Iterable.generate(count, (index)
-                  {
-                      return new Expanded(flex: flex,
-                              child: new Padding(padding: const EdgeInsets.all(1.0),
-                                            child: FlatButton(onPressed: () => numberPressed(from + index), color: Colors.white,
-                                                  child: Text("${from + index}", style: TextStyle(fontSize: 40.0),)),
-                              ),
-                      );
-                  }).toList();
-  }
-
-  Widget buildOperatorButton(String label, OperatorFunc func, int flex, {Color color = Colors.amber}){
-      return Expanded(flex: flex,
-              child: Padding(
-                  padding: const EdgeInsets.all(1.0),
-                  child: FlatButton(onPressed: () => calc(func), color: color, 
-                      child: Text(label, style: TextStyle(fontSize: 40.0),)),
-                      ),
+  List<Widget> buildNumberButtons(int count, int from, int flex) {
+    return new Iterable.generate(count, (index) {
+      return new Expanded(flex: flex,
+        child: new Padding(padding: const EdgeInsets.all(8.0),
+          child: RaisedButton(onPressed: () => numberPressed(from + index),
+              color: const Color(0xffeeffff), highlightColor: const Color(0xffaaddaa),
+              elevation: 8.0, highlightElevation: 12.0,
+              child: Text(
+                "${from + index}", style: TextStyle(fontSize: 40.0),)),
+        ),
       );
+    }).toList();
+  }
+
+  Widget buildOperatorButton(String label, OperatorFunc func, int flex,) {
+    return Expanded(flex: flex,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: RaisedButton(onPressed: () => calc(func),
+            color: const Color(0xffffddff), highlightColor: const Color(0xffffaaff), elevation: 10.0, highlightElevation: 16.0,
+            child: Text(label,style:  new TextStyle(fontSize: 32.0))),
+      ),
+    );
   }
 
   Widget buildRow(int numberKeyCount, int startNumber,  int numberFlex, String operationLabel, OperatorFunc operation, int operrationFlex){
-      return new Expanded(child: 
+      return new Expanded(child:
                 Row(crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: new List.from(buildNumberButtons(numberKeyCount,startNumber ,numberFlex,))
                                     ..add(buildOperatorButton(operationLabel, operation, operrationFlex))));
@@ -70,17 +70,18 @@ class MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(      
+    return new MaterialApp(
       home: new SafeArea(
-              child: new Material(color: Colors.black,
+              child: new Material(color: const Color(0xffffeeff),
                 child: Column( crossAxisAlignment: CrossAxisAlignment.end,
                    children: <Widget>[
-            Expanded(child: Row(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.end, children: [Text(resultString,textAlign: TextAlign.right,  style: TextStyle(fontSize: 50.0, color: Colors.white),)])),
+            Expanded(child: new Card(child:Row(crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.end,
+                children: [Text(resultString,textAlign: TextAlign.right, style: TextStyle(fontSize: 50.0, color: const Color(0xff114411)),)]))),
             buildRow(3,7,1,"/", (accu, dividor)=> accu / dividor , 1),
             buildRow(3,4,1,"x", (accu, dividor)=> accu * dividor , 1),
             buildRow(3,1,1,"-", (accu, dividor)=> accu - dividor , 1),
             buildRow(1, 0,3,"+", (accu, dividor)=> accu + dividor , 1),
-            Expanded(child:Row(crossAxisAlignment: CrossAxisAlignment.stretch,children: [buildOperatorButton("C", null, 1, color: Colors.grey),buildOperatorButton("=", (accu, dividor)=> accu, 3)],))
+            Expanded(child:Row(crossAxisAlignment: CrossAxisAlignment.stretch,children: [buildOperatorButton("C", null, 1,),buildOperatorButton("=", (accu, dividor)=> accu, 3)],))
           ],),
         ),
       )
